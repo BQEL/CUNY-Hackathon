b0cdf88091394b1e6ab9bc914a9038eaa5ea756e
kr3110 <n.nkr3110@gmail.com>
2017-04-30 10:47:29 -0400

    vision testing

--- Routes/router.js
+++ Routes/router.js
@@ -1,20 +1,66 @@
-var express= require('express');
+var express = require('express');
 var router = express.Router();
 var passport = require('passport');
-var jwt= require('jwt-simple');
-var Psych= require('../app/models/user');
-var patient=require('../app/models/patient');
-var AccessToken=require('twilio').jwt.AccessToken;
-var VideoGrant=AccesToken.VideoGrant;
-
+var jwt = require('jwt-simple');
+var Psych = require('../app/models/userlogin');
+var patient = require('../app/models/patientlogin');
+//var AccessToken = require('twilio').jwt.AccessToken;
+//var VideoGrant = AccesToken.VideoGrant;
+//google visio
+var vision = require('@google-cloud/vision')({
+    projectId: 'api-project-420473628958'
+    , keyFilename: 'visionkey.json'
+});
 require('../config/passport')(passport);
-
-
+//patient sign
+router.post('/patientSign', function (req, res) {
+    if (!req.body.name || !req.body.password || req.body.psych) {
+        res.json({
+            success: false
+            , msg: 'Please pass Patient fields'
+        });
+    }
+    else {
+        Psych.findOne({
+            email: req.body.email
+        }, function (err, user) {
+            if (err) throw err;
+            if (!user) {
+                res.send({
+                    success: false
+                    , msg: 'Authentication failed. Doctor username was not found.'
+                });
+            }
+            else {
+                var newUser = new patient({
+                    user: user.body.username
+                    , password: req.body.password
+                    , name: req.body.name
+                    , lastname: req.body.lastname
+                    , psych: req.body.psych
+                });
+                // save the user
+                newUser.save(function (err) {
+                    if (err) {
+                        return res.json({
+                            success: false
+                            , msg: 'username exists.'
+                        });
+                    }
+                    res.json({
+                        success: true
+                        , msg: 'Successful created new user.'
+                    });
+                });
+            }
+        });
+    }
+});
 //singup Psych
 router.post('/signup', function (req, res) {
     if (!req.body.name || !req.body.password) {
         res.json({
-             success: false
+            success: false
             , msg: 'Please pass user fields'
         });
     }
@@ -40,9 +86,7 @@ router.post('/signup', function (req, res) {
         });
     }
 });
-
 //authentication Psych
-
 router.post('/authenticate', function (req, res) {
     User.findOne({
         email: req.body.email
@@ -110,10 +154,64 @@ router.get('/personalInfo', passport.authenticate('jwt', {
         });
     }
 });
-
-
-
-
+router.post('/unsecure', function (req, res) {
+    fs.readFile(req.files.image.path, function (err, data) {
+        var imageName = req.files.image.name
+            // If there's an error
+        if (!imageName) {
+            console.log("There was an error")
+            res.redirect("/");
+            res.end();
+        }
+        else {
+            vision.detectText('./image.jpg', function (err, text) {
+                res.send(text);
+            });
+        }
+    });
+});
+//upload screenshoot secure route
+router.post('/upload', passport.authenticate('jwt', {
+    session: false
+}), function (req, res) {
+    var token = getToken(req.headers);
+    if (token) {
+        var decoded = jwt.decode(token, config.secret);
+        Psych.findOne({
+            email: decoded.email
+        }, function (err, user) {
+            if (err) throw err;
+            if (!user) {
+                return res.status(403).send({
+                    success: false
+                    , msg: 'Authentication failed. Psych not found.'
+                });
+            }
+            else {
+                fs.readFile(req.files.image.path, function (err, data) {
+                    var imageName = req.files.image.name
+                        // If there's an error
+                    if (!imageName) {
+                        console.log("There was an error")
+                        res.redirect("/");
+                        res.end();
+                    }
+                    else {
+                        vision.detectText('./image.jpg', function (err, text) {
+                            res.send(text);
+                        });
+                    }
+                });
+            }
+        });
+    }
+    else {
+        return res.status(403).send({
+            success: false
+            , msg: 'No token provided.'
+        });
+    }
+});
 //get token
 getToken = function (headers) {
     if (headers && headers.authorization) {
@@ -129,5 +227,4 @@ getToken = function (headers) {
         return null;
     }
 };
-
-module.exports=router;
+module.exports = router;
